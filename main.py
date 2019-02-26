64ac8e1e2cd5fd1aaab6917c08a365a3efc052ca
Brandon Pon <brandonpon1337@gmail.com>
2019-02-26 09:41:07 -0800

    Added RAFT files (excluding log and configs)

--- main.py
+++ main.py
@@ -16,18 +16,27 @@ class Transaction(object):
 		transaction = "money_transfer({}, {}, {})".format(self.amount, self.source, self.destination)
 		return transaction
 
-
 class Block(object):
 	def __init__(self, transactions, proposer):
 		self.transactions = transactions
 		self.proposer = str(proposer)
+		self.header
 
 	# Function below may not work due to class iterator (quick fix: use pointer in a method outside of this class)
 	# def print_block(self):
 	# 	for transaction in self.transactions:
 	# 		print(str(transaction))
 
+# TODO: complete blockchain object
+class BlockChain(object):
+	def __init__(self, block):
+		self.blockchain = []
+
+	# TODO: print current blockchain
+	def print_blockchain():
+		pass
+
 def parse_config(json_file):
 	with open(str(json_file), "r") as file:
-		# parse and assign to global values
+		# TODO: parse and assign to global values
 		return
