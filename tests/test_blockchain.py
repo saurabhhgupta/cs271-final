81b412392f23759215777c1d310c6b2b1c749a49
Saurabh Gupta <guptahsaurabh@gmail.com>
2019-02-28 09:23:41 -0800

    Add blockchain unit test (not working)

---
+++ tests/test_blockchain.py
@@ -0,0 +1,9 @@
+import unittest
+from .. import blockchain
+
+class TestBlockchain(unittest.TestCase):
+    def test_init_chain(self):
+        self.assertEqual(initBlockChain(CHAIN_INIT_FILE), '[A B 5, B C 5, A C 10, C B 10, B A 15, C A 15, A B 20]')
+
+if __name__ == "__main__":
+    unittest.main()
